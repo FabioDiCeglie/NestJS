22e9b1bc092fd764622e270ed22e74ce16ebcd0e
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-15 18:12:01 +0100

    install argon and write steps for signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    this.prisma;
+  signup(dto: AuthDto) {
+    //generate the pass
+    //save the new user
+    //return the saved user
   }
   signin() {
     return 'I am signed in';
