eb24a9024a20c21926eaa1ac891d8fd339e07154
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-15 17:53:48 +0100

    using express

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signup() {
-    return 'I am sign up';
+    this.prisma;
   }
   signin() {
     return 'I am signed in';
