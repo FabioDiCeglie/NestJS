8084ce311e7ed1ae275a6a30f0a006349538979f
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-15 18:02:03 +0100

    use parse int pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -7,7 +7,10 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
     return this.authService.signup;
   }
 
