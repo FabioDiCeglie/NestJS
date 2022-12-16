11e2708a6cfc6043f91d746702a9a131834ec3ca
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-16 11:18:06 +0100

    return an access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -56,16 +56,23 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ accessToken: string }> {
     const payload = {
       sub: userId,
       email: email,
     };
     const secret = this.config.get('JWT_SECRET');
 
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret,
     });
+
+    return {
+      accessToken: token,
+    };
   }
 }
