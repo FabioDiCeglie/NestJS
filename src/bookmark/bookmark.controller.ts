c76d40fa9953454ce1b5857cd03a47661c27d534
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-27 16:43:07 +0100

    start working on bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,14 +1,26 @@
 import { JwtGuard } from '@/auth/guard';
-import { Controller, Delete, Get, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { BookmarkService } from './bookmark.service';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
   @Get()
   getBookmarks() {}
 
   @Get()
   getBookmarkById() {}
+
+  @Post()
+  createBookmarks() {}
 
   @Patch()
   editBookmarkById() {}
