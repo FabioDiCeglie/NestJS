90ec1a974eabd59928f01d1d6998d394647fafe5
FabioDiCeglie <dcfabio96@gmail.com>
2022-12-27 16:56:20 +0100

    setting the logic of bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -20,27 +20,39 @@ export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
   @Get()
   getBookmarks(@GetUser('id') userId: number) {
-    this.bookmarkService.getBookmarks();
+    return this.bookmarkService.getBookmarks(userId);
   }
 
   @Get(':id')
   getBookmarkById(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) bookmarkId: number,
-  ) {}
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
 
   @Post()
   createBookmarks(
     @GetUser('id') userId: number,
     @Body() dto: CreateBookmarkDto,
-  ) {}
+  ) {
+    return this.bookmarkService.createBookmarks(userId, dto);
+  }
 
-  @Patch()
+  @Patch(':id')
   editBookmarkById(
     @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
     @Body() dto: EditBookmarkDto,
-  ) {}
+  ) {
+    return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
 
-  @Delete()
-  deleteBookmarkById(@GetUser('id') userId: number) {}
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
 }
